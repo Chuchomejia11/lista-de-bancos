b254a77e82ee1b6b55d57fe9a8ca79c89c614aed
Chuchomejia11 <jjesusmejiam11@gmail.com>
2023-11-09 10:07:56 -0600

    persistencia de estados

--- components/information/banklist/banklist.tsx
+++ components/information/banklist/banklist.tsx
@@ -1,7 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@/redux/store';
 import { loadBanks } from '@/redux/slices/sliceBanks';
+import './bankList.css'; // Importa el archivo de estilos CSS
+import { setDisplayedBank } from '@/redux/slices/sliceBankDisplayed';
+import { BankCard } from '@/components/information/bankCard/BankCard';
 
 type Bank = {
   bankName: string;
@@ -13,10 +16,11 @@ type Bank = {
 export const BankList: React.FC = () => {
   const dispatchEvent = useDispatch();
   const BankList = useSelector((state: RootState) => state.banks.listBanks);
+  const [ selectedBank, setSelectedBank ] = useState<Bank | null>(null);
 
   useEffect(() => {
     if (BankList.length === 0) {
-      const apiUrl = '/api/banks'; // Utiliza la función proxy en lugar de la URL directa
+      const apiUrl = '/api/banks';
 
       fetch(apiUrl)
         .then((response) => {
@@ -32,21 +36,29 @@ export const BankList: React.FC = () => {
           console.error('Error fetching banks:', error);
         });
     }
-  }, [dispatchEvent, BankList]);
+  }, [ dispatchEvent, BankList ]);
 
   return (
     <div>
-      <h1>Lista de Bancos</h1>
-      <ul>
+      <div className="bank-list-container">
         {BankList.map((bank: any) => (
-          <div key={bank.description}>
-            <h3>{bank.bankName}</h3>
-            <p>{bank.description}</p>
-            <p>Edad: {bank.age}</p>
-            <img src={bank.url} alt={bank.bankName} />
+          <div key={bank.description} className="bank-card">
+            <img src={bank.url} alt={bank.bankName} className="bank-image" />
+            <h3 className="bank-name">{bank.bankName}</h3>
+            <button
+              className="view-more-button"
+              onClick={() => {
+                setSelectedBank(bank);
+                dispatchEvent(setDisplayedBank(bank));
+              }}
+            >
+              Ver más
+            </button>
           </div>
         ))}
-      </ul>
+
+      </div>
+      {selectedBank && <BankCard bank={selectedBank} />}
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 import { Provider } from 'react-redux';
 
 import store from '@/redux/store'; // Importa la tienda de Redux como una importación predeterminada
@@ -9,6 +10,12 @@ const HomePage: React.FC = () => {
   return (
     <Provider store={store}>
       <div>
+        <Head>
+          <title>Vista de bancos</title>
+          <meta name="description" content="Vista de una lista de bancos con almacenamiento de estado." />
+          <meta name="author" content="J. Jesús Mejia M." />
+          
+        </Head>
         <h1>Bienvenido a mi aplicación</h1>
         <BankList />
       </div>
