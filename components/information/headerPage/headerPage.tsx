4612e872a615435367b91f708246eedc82d16181
Chuchomejia11 <jjesusmejiam11@gmail.com>
2025-01-01 14:05:12 -0600

    Update headerPage.tsx

--- components/information/headerPage/headerPage.tsx
+++ components/information/headerPage/headerPage.tsx
@@ -1,12 +1,12 @@
 export const HeaderPage: React.FC = () => {
     return (
         <div>
-        <Head>
+        <div style={{alignItems:"center", margin:"auto" }}>
             <title>Información de la página</title>
             <meta name="description" content="Vista de una página de información con almacenamiento de
             estado." />
             <meta name="author" content="J. Jesús Mejia M." />
-        </Head>
+        </div>
         <h1>Información de la página</h1>
         <p>Esta es una página de información con almacenamiento de estado.</p>
         </div>
